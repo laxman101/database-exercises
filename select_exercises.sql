9c8ed81cf6b739d02492056aa3364e1750134629
Laxman <laxman.lamichhane79@gmail.com>
2021-09-24 23:58:45 -0500

    Initial Comment

---
+++ select_exercises.sql
@@ -0,0 +1,29 @@
+Exercises
+
+-- Create a new file called select_exercises.sql. Store your code for this exercise in that file. You should be testing your code in Sequel Ace as you go.
+
+-- Use the albums_db database.
+
+-- Explore the structure of the albums table.
+
+-- a. How many rows are in the albums table?
+
+-- b. How many unique artist names are in the albums table?
+
+-- c. What is the primary key for the albums table?
+
+-- d. What is the oldest release date for any album in the albums table? What is the most recent release date?
+
+-- Write queries to find the following information:
+
+-- a. The name of all albums by Pink Floyd
+
+-- b. The year Sgt. Pepper's Lonely Hearts Club Band was released
+
+-- c. The genre for the album Nevermind
+
+-- d. Which albums were released in the 1990s
+
+-- e. Which albums had less than 20 million certified sales
+
+-- f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
